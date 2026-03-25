960a9cc137fb1d5104951a68375b6d47169e3c80
Wuanma <Wuanmaw@gmail.com>
2026-03-25 15:34:48 -0600

    modificacionapp.js

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.get('/',(req,res)=>{
+app.get('/maestro',(req,res)=>{
     res.sendFile(path.join(__dirname, 'views', 'maestro','dashboard.html'))
 });
 
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.join(__dirname, 'views', 'director','dashboard.html'))
-// });
+app.get('/director',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'director','dashboard.html'))
+});
 
 
 
