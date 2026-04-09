a1eda9534f15ddb435c7ffbcf32cf56808d67a5e
Wuanma <Wuanmaw@gmail.com>
2026-04-09 14:58:43 -0600

    estructura

--- src/app.ts
+++ src/app.ts
@@ -28,18 +28,6 @@ app.get('/director',(req,res)=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
